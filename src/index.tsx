118d7e76780e6c9e1de1a557454e0c9b22c6cbda
Michael Angerer <michael.angerer@docent.CENTIGRADE>
2020-09-25 12:21:07 +0200

    reverted github pages prep

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename='/my-app'>
+    <Router basename='/'>
       <ViewNavigator></ViewNavigator>
     </Router>
   </React.StrictMode>,
